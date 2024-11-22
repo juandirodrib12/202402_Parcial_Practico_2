bb23092ce00433271fb4e99968be2bb8909d2ad8
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2024-11-21 21:21:24 -0500

    taller angular ready

--- mynewapp/src/app/serie/serie.component.ts
+++ mynewapp/src/app/serie/serie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { SerieService } from './serie.service';
 
 @Component({
   selector: 'app-serie',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SerieComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serieService: SerieService) { }
+
+  series: Array<Serie> = []
+  selectedSerie: Serie | null = null;
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(series => this.series = series);
+  }
 
   ngOnInit() {
+    this.getSeries();
+  }
+
+  showDetails(serie: Serie): void {
+    this.selectedSerie = serie;
+  }
+
+  closeModal(): void {
+    this.selectedSerie = null;
+  }
+
+  calculateAverageSeasons(): number {
+    const totalSeasons = this.series.reduce((acc, serie) => acc + serie.seasons, 0);
+    return parseFloat((totalSeasons / this.series.length).toFixed(2));
   }
 
 }
